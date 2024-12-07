06b7ba06ba8891b9361edb06b9c77a210c72e696
daeyun <daeyun@daeyun-ubuntu22.04>
2024-12-07 15:36:21 +0900

    update for script file

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # Robot-Hand-Tracking-ROS2
 Hand tracking using camera and 6-axis robot arm with ROS2
+
+## Demo with the simulation
+#### Visual servoing
+Test on Doosan Robot simulation
+![Image Description](https://github.com/Bigyuun/teleop_robot_ws/blob/humble/media/ros2_system_config.png)
+
+
+---
+## Index
+1. [Prerequisite](#prerequisite)
+2. [System Requirements](#system-requirements)
+4. [Installation](#installation)
+6. [Node & Topic](#node--topic)
+9. [Execute](#execute)
+
+## Prerequisite
+#### OS
+Ubuntu 22.04.6 LTS
+#### Software
+- ROS2 humble
+  <https://docs.ros.org/en/humble/index.html>
+
+## System Requirements
+- Ubuntu 22.04 (ROS2 humble) or Ubuntu 20.04 (ROS2 foxy)
+- Realsense camera (D415)
+
+## Installation
+##### python
+```bash
+  $ pip install pyserial mediapipe pyrealsense2
+```
+
+## Node & Topic
+- **'handpose_node'**
+  - Publisher<br/>
+    `mediapipe_hand_landmarks`<br/>
+    `camera_to_hand_vector`<br/>
+
+## Execute
+#### Clone repository and move to this project
+```
+git clone https://github.com/DaeyunJang/Robot-Hand-Tracking-ROS2.git
+cd Robot-Hand-Tracking-ROS2
+```
+
+#### Build using colcon
+```
+colcon build --symlink-install
+```
+
+#### Source setup file
+```
+. install/setup.bash
+```
+
+#### Run
+```
+ros2 run handpose_pkg handpose_publisher
+ros2 topic echo /camera_to_hand_vector
+```
+
+## Execute (python)
+You can run the code without ROS2 system.
+It will calculate the center of the hand palm and its normal vector. This position and normal vector can change to 6D coordinate [x,y,z,R,P,Y].
+Finally these data will send through the TCP communication.
+
+```
+cd {PROJECT_DIRECTORY}/scripts
+```
+```
+python3 main.py
+```
+#### main.py
+These codes consist of 3 processes
+1. Find the hand landmarks and calculate the palm center and its `normal vector` in the real-world.
+2. Show plot using the hand landmarks.
+3. Send the `target pose of the end-effector` as 6D Euler [x,y,z,R,P,Y] othrough the TCP comminication. 
+
+
+
+
+## Note
+Imagine, you have the 6D robot arm and the camera mounted on the end-effector of this robot.
+This code can find the hand pose using Google Mediapipe, and Realsense camera will calculate the real-world [x,y,z] of 'finger tip'. Finally, ROS2 topic will publish these coordinate values.
+-> As you can expect, [x,y,z] means the distance from the center of the camera
+
+I implemented that this ROS system send(publish) the topic 'camera_to_hand_vector' as real-world [x,y,z] of 'finger tip'. (handpose_indexfinger_publisher.py)
+
+If you want to move the 6D robot arm in the simulation or real-world,
+you can subscribe 'camera_to_hand_vector' and move relative its end-effector by [x, y, z].
+
+If you want to publish all hand pose data,
+you will develop the code. (the file 'handpose_publisher.py' in src/handpose_pkg/handpose_pkg is not applied ROS2 system)
+
+### Debug
+Please check the camera depth unit option(line 171 of 'handpose_indexfinger_publisher.py') because the unit(mm) can be different for each model.
+If you don't use the Realsense camera, you have to modify the code for capturing the camera frame.
+
--- common/mediapipe_api/handlandmarks/handlandmarks.py
+++ common/mediapipe_api/handlandmarks/handlandmarks.py
@@ -7,6 +7,8 @@ import os
 import sys
 import json
 import queue
+
+from scipy.spatial.transform import Rotation as R
 queue = queue.Queue()
 
 class HandLandmarks():
@@ -21,6 +23,11 @@ class HandLandmarks():
         self.hand_pose['label'] = None
         self.hand_pose['landmarks'] = np.zeros((21, 3))
         self.hand_pose['world_landmarks'] = np.zeros((21, 3))  # calculate from camera instrinsic(Realsense)
+        self.hand_pose['palm_pose_xyz_normvector'] = np.zeros((6,1))  # calculate from camera instrinsic(Realsense)
+        self.hand_pose['palm_pose_xyz_normvector_stabilization'] = np.zeros((6,1))  # calculate from camera instrinsic(Realsense)
+        self.hand_pose['palm_pose_xyz_euler_ZYZ'] = np.zeros((6,1))  # calculate from camera instrinsic(Realsense)
+        self.hand_pose['palm_pose_xyz_euler_ZYZ_robot'] = np.zeros((6,1))  # calculate from camera instrinsic(Realsense)
+        self.hand_pose['good_hand_error'] = 100  # calculate from camera instrinsic(Realsense)
         self.hand_results = []
         pass
 
@@ -40,9 +47,16 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
         self.replace_points = np.zeros((21, 3))
         self.replace_threshold = replace_threshold
 
-        self.palm_vector = {'x', 'y', 'z', 'r', 'p', 'y'}
+        self.palm_vector = {'x': 0.0, 'y': 0.0, 'z': 0.0, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0}
 
         self.depth_intrinsics = None
+
+        self.e2c_r = np.array([[0.99978086, -0.01960594, -0.00733774],
+                               [0.01955018,  0.99978003, -0.00759461],
+                               [0.00748503,  0.00744949,  0.99994424]])
+        self.e2c_t = np.array([[-0.00181539],
+                               [-0.08517568],
+                               [0.02586356]])
 
         self.image_width = image_width
         self.image_height = image_height
@@ -112,8 +126,11 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
                 self.hand_pose['landmarks'] = self.hand_landmarks
                 if self.depth_intrinsics != None:
                     self.hand_pose['world_landmarks'] = self.get_hand_world_xyz()
-                c, n_v = self.get_palm_pose(coord='replace')
-                c2, n_v2 = self.get_palm_pose(coord='canonical')
+
+                if classification.index != None:
+                    c, n_v = self.get_palm_pose(coord='replace')
+                    zyz = self.hand_pose['palm_pose_xyz_euler_ZYZ']
+                    # print(f'{zyz}')
 
                 # finally
                 self.hand_results.append(self.hand_pose)
@@ -131,6 +148,12 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
                 img_depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.3),
                                                             cv2.COLORMAP_JET)
                 drawing_depth_image = self.depth_image_colormap
+
+                text_palm_pose = self.hand_pose['palm_pose_xyz_normvector']
+                text_palm_pose_stabilzed = self.hand_pose['palm_pose_xyz_normvector_stabilization']
+                text_palm_pose_zyz = self.hand_pose['palm_pose_xyz_euler_ZYZ']
+                text_palm_pose_zyz_robot = self.hand_pose['palm_pose_xyz_euler_ZYZ_robot']
+                good_hand_error = self.hand_pose['good_hand_error']
                 self.mp_drawing.draw_landmarks(
                     drawing_depth_image,
                     hand_landmarks,
@@ -140,20 +163,64 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
                 )
                 drawing_image = cv2.putText(img=drawing_image,
                                             text=f'Depth(avg) = {depth_avg}',
-                                            org=(50, 50),
+                                            org=(30, 30),
                                             fontFace=cv2.FONT_HERSHEY_PLAIN,
                                             fontScale=1,
                                             color=(0,255,0),
-                                            thickness=2,
+                                            thickness=1,
                                             lineType=cv2.LINE_AA)
                 drawing_image = cv2.putText(img=drawing_image,
-                                            text=f'index finger depth = {world_points[7,2]}',
-                                            org=(50, 80),
+                                            # text=f'index finger xyz (pixel)= {world_points[7,0]}, {world_points[7,1]}, {world_points[7,2]} m',
+                                            text=f'index finger xyz (pixel)= {replace_points[7,0]:.4f}, {replace_points[7,1]:.4f}, {replace_points[7,2]:.4f} m',
+                                            org=(30, 50),
                                             fontFace=cv2.FONT_HERSHEY_PLAIN,
                                             fontScale=1,
                                             color=(0, 255, 0),
-                                            thickness=2,
+                                            thickness=1,
                                             lineType=cv2.LINE_AA)
+                drawing_image = cv2.putText(img=drawing_image,
+                                            # text=f'index finger xyz (pixel)= {world_points[7,0]}, {world_points[7,1]}, {world_points[7,2]} m',
+                                            text=f'normal vector: {text_palm_pose[3]:.3f}, {text_palm_pose[4]:.3f}, {text_palm_pose[5]:.3f}',
+                                            org=(30, 70),
+                                            fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                            fontScale=1,
+                                            color=(0, 255, 0),
+                                            thickness=1,
+                                            lineType=cv2.LINE_AA)
+                drawing_image = cv2.putText(img=drawing_image,
+                                            # text=f'index finger xyz (pixel)= {world_points[7,0]}, {world_points[7,1]}, {world_points[7,2]} m',
+                                            text=f'normal vector(stabilization): {text_palm_pose_stabilzed[3]:.3f}, {text_palm_pose_stabilzed[4]:.3f}, {text_palm_pose_stabilzed[5]:.3f}',
+                                            org=(50, 90),
+                                            fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                            fontScale=1,
+                                            color=(0, 255, 0),
+                                            thickness=1,
+                                            lineType=cv2.LINE_AA)
+                drawing_image = cv2.putText(img=drawing_image,
+                                            text=f'zyz(CAMERA)= {np.round(text_palm_pose_zyz,2)}',
+                                            org=(30, 110),
+                                            fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                            fontScale=1,
+                                            color=(0, 255, 0),
+                                            thickness=1,
+                                            lineType=cv2.LINE_AA)
+                drawing_image = cv2.putText(img=drawing_image,
+                                            text=f'(ROBOT)= {np.round(text_palm_pose_zyz,2)}',
+                                            org=(30, 130),
+                                            fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                            fontScale=1,
+                                            color=(0, 255, 55),
+                                            thickness=1,
+                                            lineType=cv2.LINE_AA)
+                drawing_image = cv2.putText(img=drawing_image,
+                                            text=f'Good Hand error= {good_hand_error} mm',
+                                            org=(30, 150),
+                                            fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                            fontScale=1,
+                                            color=(255, 0, 55),
+                                            thickness=1,
+                                            lineType=cv2.LINE_AA)
+
                 self.drawing_image = drawing_image
                 self.drawing_depth_image = drawing_depth_image
 
@@ -202,6 +269,44 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
         replace_points[xyz_distances >= self.replace_threshold] = canonical_point[xyz_distances >= self.replace_threshold]
         return canonical_point, world_point, world_point_iqr, replace_points, depth_avg
         pass
+
+    def palmpose_to_euler(self, norm_vector, method='ZYZ', ref=None):
+        # extract x, y, z values
+        x, y, z = norm_vector
+
+        # cos_0과 sin_0 계산
+        cos_0 = np.sqrt(x ** 2 + y ** 2)
+        sin_0 = z
+        # cos_1과 sin_1 계산
+        cos_1 = x / cos_0
+        sin_1 = y / cos_0
+        # RyRz 행렬 생성
+        RyRz = np.array([
+            [x, -sin_0, -sin_0 * cos_1],
+            [y, cos_0, -sin_0 * sin_1],
+            [z, 0, cos_0]
+        ])
+
+        Rx = R.from_euler('x', angles=0, degrees=False)
+        if ref != None:
+            Rx = R.from_euler('x', angles=ref, degrees=False)
+
+        Rx = Rx.as_matrix()
+
+        R_2 = Rx * RyRz
+        Matrix_robot = R.from_matrix(self.e2c_r.T @ R_2)
+        R_2 = R.from_matrix(Rx @ RyRz)
+
+        euler = None
+        if method=='ZYZ':
+            euler = R_2.as_euler('zyz', degrees=True)
+            euler_robot = Matrix_robot.as_euler('zyz', degrees=True)
+        elif method=='XYZ':
+            euler = R_2.as_euler('xyz', degrees=True)
+            euler_robot = Matrix_robot.as_euler('xyz', degrees=True)
+            pass
+
+        return euler, euler_robot
 
     @staticmethod
     def vector_to_rpy(x, y, z):
@@ -294,7 +399,7 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
         xyz_world = np.asarray(xyz_world, dtype=np.float64)
         return xyz_world
 
-    def get_palm_pose(self, coord='replace'):
+    def get_palm_pose(self, coord='replace', ref='world_coordinate'):
         """
         Find some points of hand landmarks
         :param keypoint: handlandmark_keypoints.json
@@ -305,11 +410,77 @@ class MediaPipeHandLandmarkDetector(HandLandmarks):
         p2 = self.get_hand_keypoint_pixel_xyz(hand_index=0, keypoint='pinky_mcp', coord=coord)
         p3 = self.get_hand_keypoint_pixel_xyz(hand_index=0, keypoint='index_finger_mcp', coord=coord)
 
+        # normal vector stabilization
+        p1_canonical = self.get_hand_keypoint_pixel_xyz(hand_index=0, keypoint='wrist', coord='canonical')
+        p2_canonical = self.get_hand_keypoint_pixel_xyz(hand_index=0, keypoint='pinky_mcp', coord='canonical')
+        p3_canonical = self.get_hand_keypoint_pixel_xyz(hand_index=0, keypoint='index_finger_mcp', coord='canonical')
+
+        if ref=='world_coordinate':
+            p1 = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p1[0], p1[1]], p1[2])
+            p2 = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p2[0], p2[1]], p2[2])
+            p3 = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p3[0], p3[1]], p3[2])
+            p1 = np.array(p1)
+            p2 = np.array(p2)
+            p3 = np.array(p3)
+
+
+            p1_canonical = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p1_canonical[0], p1_canonical[1]], p1_canonical[2])
+            p2_canonical = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p2_canonical[0], p2_canonical[1]], p2_canonical[2])
+            p3_canonical = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [p3_canonical[0], p3_canonical[1]], p3_canonical[2])
+            p1_canonical = np.array(p1_canonical)
+            p2_canonical = np.array(p2_canonical)
+            p3_canonical = np.array(p3_canonical)
+
+        else: # e.g. ref is pixel coordinate
+            p1 = p1
+            p2 = p2
+            p3 = p3
+
         v1 = p2 - p1
         v2 = p3 - p1
+        norm_v1_size = np.linalg.norm(v1)
+        norm_v2_size = np.linalg.norm(v2)
+
         center_point = (p1 + p2 + p3) / 3.
+
+        prev_center_point = np.array([self.palm_vector['x'], self.palm_vector['y'], self.palm_vector['z']])
+        distance = np.linalg.norm(center_point - prev_center_point)
+        # print(f'p1:{p1} / p2:{p2} / p3:{p3} / ct: {center_point} / ct_p: {prev_center_point} err:{distance}')
+        self.hand_pose['good_hand_error'] = distance
+
         normal_vector = np.cross(v1, v2)
         normal_unit_vector = normal_vector / np.linalg.norm(normal_vector, 2)
+
+        self.palm_vector['x'] = center_point[0]
+        self.palm_vector['y'] = center_point[1]
+        self.palm_vector['z'] = center_point[2]
+        self.palm_vector['roll'] = normal_unit_vector[0]
+        self.palm_vector['pitch'] = normal_unit_vector[1]
+        self.palm_vector['yaw'] = normal_unit_vector[2]
+
+        self.hand_pose['palm_pose_xyz_normvector'] = np.concatenate((center_point, normal_unit_vector))
+
+        ##########################################################
+        # DY
+        # Trick for stablization normal vecotr on palm center
+        # just using points in normalized coordinate (just mediapipe plane)
+        v1_canonical = p2_canonical - p1_canonical
+        v2_canonical = p3_canonical - p1_canonical
+        norm_v1_size = np.linalg.norm(v1_canonical)
+        norm_v2_size = np.linalg.norm(v2_canonical)
+        normal_vector_canonical = np.cross(v1_canonical, v2_canonical)
+        normal_unit_vector_canonical = normal_vector_canonical / np.linalg.norm(normal_vector_canonical, 2)
+        self.hand_pose['palm_pose_xyz_normvector_stabilization'] = np.concatenate((center_point, normal_unit_vector_canonical))
+        ##########################################################
+
+        dot_product = np.dot(v1, v2)
+        cos_theta = dot_product / (norm_v1_size * norm_v2_size)
+        theta = np.arccos(cos_theta) / 2.0
+        euler, robot_euler = self.palmpose_to_euler(norm_vector=-normal_unit_vector, method='ZYZ', ref=None)
+
+        self.hand_pose['palm_pose_xyz_euler_ZYZ'] = np.concatenate((center_point, euler))
+        robot_center_point = self.e2c_t.T[0] + (self.e2c_r @ center_point[np.newaxis, :].T).T[0]
+        self.hand_pose['palm_pose_xyz_euler_ZYZ_robot'] = np.concatenate((robot_center_point, robot_euler))
 
         return center_point, normal_unit_vector
 
